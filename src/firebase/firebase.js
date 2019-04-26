e1e6f3ea22a7b32144d8b396ed24d5040364d6c1
shawnSFU <shawn.noruzi@gmail.com>
2019-04-26 00:46:52 -0700

    Creation of Test DB
    To avoid wiping real user data - we need a test DB.
    
    Added cross-env package to allow cross platform (OS) management for our tests.
    
    dotenv package to read firebase environment files.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,12 +7,12 @@ import * as firebase from 'firebase';
 // Add the Firebase services that you want to use
 
 const config = {
-  apiKey: "AIzaSyCylMs8RB5Qq2C5wJyCdOZ1srqiAWktKsE",
-  authDomain: "expensify-5b694.firebaseapp.com",
-  databaseURL: "https://expensify-5b694.firebaseio.com",
-  projectId: "expensify-5b694",
-  storageBucket: "expensify-5b694.appspot.com",
-  messagingSenderId: "160049049295"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain:  process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL:  process.env.FIREBASE_DATABASE_URL,
+  projectId:  process.env.FIREBASE_PROJECT_ID,
+  storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 
