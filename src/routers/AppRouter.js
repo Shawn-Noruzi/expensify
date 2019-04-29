a1056e4a35c27c81787717c1bb0584ec8b68cbab
shawnSFU <shawn.noruzi@gmail.com>
2019-04-28 23:01:08 -0700

    Added PublicRoute to our login Page
    Dashboard doesnt send you back to the login page anymore.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import { Router, Route, Switch } from 'react-router-dom';
 import AddExpensePage from "./../components/AddExpense";
 import EditExpensePage from "./../components/EditExpense";
 import ExpenseDashboardPage from "./../components/ExpenseDashboard";
-import HelpPage from "./../components/Help";
 import LoginPage from './../components/Login';
 import NotFoundPage from "./../components/NotFound";
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={LoginPage} exact={true} />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
                 <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
                 <PrivateRoute path="/create" component={AddExpensePage} />
                 <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
