7be5be46ff7bd85c933780256ea463fa9b29a24b
shawnSFU <shawn.noruzi@gmail.com>
2019-05-03 12:29:47 -0700

    Styling Expensify
    - login page - putting a BG image behind the login button and centering all components.
    
    Prevented non-expense data from being submitted.
    
    Added a rule in the DB for adding expenses to prevent users from adding anything other than an expense.
    
    Issue: Add Expense button has disapeared  - likely to do with routing, will roll back and test.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import React from "react";
+import { connect } from "react-redux";
+import { startLogin } from "../actions/auth";
 
 //doesnt need access to state -> dont need to provide stateToProps
 //needs dispatchToProps -> bc we want to dispatch an action (startLogin)
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title"> Expensify</h1>
+      <button className="button" onClick={startLogin}><span>Login</span></button>
+    </div>
   </div>
 );
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   startLogin: () => dispatch(startLogin())
 });
 
-export default connect(undefined, mapDispatchToProps)(LoginPage);
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(LoginPage);
